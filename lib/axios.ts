2746357e2546295e78bac39af01a012b0999ea18
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-07-13 23:22:13 +0700

    feat: add axios instance with request interceptor for API calls

---
+++ lib/axios.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
+});
+
+api.interceptors.request.use((config) => {
+    if (typeof window !== "undefined") {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+    }
+
+    return config;
+});
